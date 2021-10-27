c5c9ef2e1a89c919d90eddfdbd0eea5fd2a9d358
Shaw-law <biswajitshaw184@gmail.com>
2021-10-27 17:21:44 +0530

    updated added two new directories

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <malloc.h>
 
 using namespace std;
 
 int main() {
-  cout << "Hello Github";
+
+  int i, num, *arr;
+
+  cout << "Please enter the enter the amount of elements in the array:";
+  cin >> num;
+
+  arr = (int *)malloc(num * sizeof(int));
+
+  cout << "\nPlease enter the element of the array:";
+
+  for (i = 0; i < num; i++) {
+    cin >> arr[i];
+  }
+
+  cout << "\nThe elements of the array are:";
+  for (i = 0; i < num; i++) {
+    cout << arr[i];
+  }
+
   return 0;
 }
