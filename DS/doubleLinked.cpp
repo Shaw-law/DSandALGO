384f7aaf292c3da72b041cbd748fcf4a83fdf307
Shaw-law <biswajitshaw184@gmail.com>
2021-11-02 16:33:07 +0530

    Added a Double Linked Program

--- DS/doubleLinked.cpp
+++ DS/doubleLinked.cpp
@@ -1,19 +1,74 @@
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 
 struct Node {
   int data;
-  struct Node *next, *prev;
+  struct Node *prev;
+  struct Node *next;
 };
 
-typedef struct Node* node;
+typedef struct Node *node;
 
-void insert(int data, node ptr) {
-  ptr = (node)malloc(sizeof(Node));
+void push(Node **head_href, int new_data) {
+  node new_node = (node)malloc(sizeof(Node));
+
+  new_node->data = new_data;
+
+  new_node->next = *head_href;
+  new_node->prev = NULL;
+
+  if ((*head_href) != NULL)
+    (*head_href)->prev = new_node;
+
+  (*head_href) = new_node;
 }
-int main() {
-  node ptr, HEAD;
 
-  insert(4,&HEAD);
+void insertBefore(node* head_href, node next_node, int new_data) {
+  if(next_node == NULL) {
+    cout<<"The given node cannot be NULL";
+    return;
+  }
+
+  node new_node = (node)malloc(sizeof(Node));
+
+  new_node->data = new_data;
+
+  new_node->prev = next_node->prev;
+  next_node->prev = new_node;
+  new_node->next = next_node;
+
+  if(new_node->prev != NULL) {
+    new_node->prev->next = new_node;
+  } else {
+    (*head_href) = new_node;
+  }
+}
+
+void printList(node leonardo) {
+  node last;
+  cout << "Transversal in forward direction";
+
+  while (leonardo != NULL) {
+    cout <<"  "<< leonardo->data;
+    last = leonardo;
+    leonardo = leonardo->next;
+  }
+}
+
+int main() {
+  node head = NULL;
+
+  push(&head, 7);
+  push(&head, 1);
+  push(&head, 4);
+
+  insertBefore(&head, head->next, 8);
+
+  cout << "created DLL is :";
+  printList(head);
+
+  getchar();
+  return 0;
 }
